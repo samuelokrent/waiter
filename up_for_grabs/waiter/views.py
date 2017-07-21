9053cfbeb7402c4dd341d0185d4ae5930b33c3c7
Yahor Yuzefovich <yahor.yuzefovich@cloudera.com>
2017-07-21 12:33:36 -0700

    Sending thank you email

--- up_for_grabs/waiter/views.py
+++ up_for_grabs/waiter/views.py
@@ -8,7 +8,10 @@ from django.views.decorators.csrf import csrf_exempt
 from waiter.models import Order
 
 import json
+import django
 from django.utils import timezone
+from django.conf import settings
+from django.core.mail import send_mail
 
 def index(request, template_name='orders/index.html'):
     return render(request, template_name, {})
@@ -24,6 +27,9 @@ def order_list(request, template_name='orders/order_list.html'):
     data['object_list'] = orders
     return render(request, template_name, data)
 
+def send_email(subject, message, email_to):
+    send_mail(subject, message, settings.EMAIL_HOST_USER, [email_to], fail_silently=False)
+
 def order_claim(request, pk):
     print("order_claim: " + str(pk))
     order = Order.objects.get(pk=pk)
@@ -31,13 +37,16 @@ def order_claim(request, pk):
     name = ''
     description = ''
     restaurant = ''
+    email = ''
     if order:
         name = order.name
         description = order.description
         restaurant = order.restaurant
+        email = order.email
         order.delete()
         claimed = True
-    return HttpResponse(json.dumps({'claimed': claimed, 'name': name, 'description': description, 'restaurant': restaurant}), content_type='application/json')
+        send_email("Thank you!", "The food that you put up-for-grabs has been claimed!", email)
+    return HttpResponse(json.dumps({'claimed': claimed, 'name': name, 'description': description, 'restaurant': restaurant, 'email': email}), content_type='application/json')
 
 @csrf_exempt
 def order_create(request):
@@ -62,6 +71,7 @@ def create_test(request):
     try:
         print "create_test"
         o = Order(description="description",
+                email='yahor.yuzefovich@cloudera.com',
                 name='name',
                 office='office',
                 arrival_time = timezone.now())
